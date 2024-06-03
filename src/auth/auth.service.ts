0416e4b4d12904d577e9d71ee21a7076c7e5ea7a
Grigogo <Grigoriirozhkov@gmail.com>
2024-06-03 18:11:31 +0300

    add JWT start

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma.service';
+import { AuthDto } from './dto/auth.dto';
+import { faker } from '@faker-js/faker';
+import { hash } from 'argon2';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  register() {
-    return {
-      name: 'Grigogo'
-    };
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService
+  ) {}
+  async register(dto: AuthDto) {
+    const oldUser = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email
+      }
+    });
+
+    if (oldUser) throw new BadRequestException('User already exists');
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        name: faker.person.firstName(),
+        avatarPath: faker.image.avatar(),
+        phone: faker.phone.number(),
+        password: await hash(dto.password)
+      }
+    });
+
+    return user;
   }
 }
